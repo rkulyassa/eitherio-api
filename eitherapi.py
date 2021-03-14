08da5bf00c58a8f6f3eed53d9d4bef42bc8c9a26
Ryan Kulyassa <73004474+rkulyassa@users.noreply.github.com>
2021-03-14 10:05:16 -0400

    reorganize

--- eitherapi.py
+++ eitherapi.py
@@ -1,38 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-class eitherio:
+class Eitherio:
 
   def __init__(self):
     res = requests.get('https://either.io').content.decode()
     self.html = BeautifulSoup(res, 'html.parser')
+    self.title = self.html.find('h2', {'id':'question-title'}).getText()
+    self.author = self.html.find('span', {'id':'question-author'}).getText()[9:]
+    self.link = self.html.find('input', {'type':'hidden'})['value']
+    self.tags = [tag.getText().replace('\n','') for tag in self.html.find_all('ul', {'class':'tags'})]
+    self.comments = int(self.html.find('span', {'class':'comment-number'}).getText().replace(',',''))
+    self.choices = self.html.find_all('span', {'class':'option-text'})
+    self.percentages = self.html.find_all('div', {'class':'percentage'})
+    self.votes = self.html.find_all('span', {'class':'count'})
 
   def get_question(self):
     '''
     Fetch various data for a random question, returns dict.
     '''
-    title = self.html.find('h2', {'id':'question-title'}).getText()
-    author = self.html.find('span', {'id':'question-author'}).getText()[9:]
-    link = self.html.find('input', {'type':'hidden'})['value']
-    tags = [tag.getText().replace('\n','') for tag in self.html.find_all('ul', {'class':'tags'})]
-    comments = int(self.html.find('span', {'class':'comment-number'}).getText().replace(',',''))
-    choices = self.html.find_all('span', {'class':'option-text'})
-    percentages = self.html.find_all('div', {'class':'percentage'})
-    votes = self.html.find_all('span', {'class':'count'})
     return {
-      'title': title,
-      'author': author,
-      'link': link,
-      'tags': tags,
-      'comments': comments,
+      'title': self.title,
+      'author': self.author,
+      'link': self.link,
+      'tags': self.tags,
+      'comments': self.comments,
       'choice_1': {
-        'text': choices[0].getText(),
-        'percentage': percentages[0].getText(),
-        'votes': int(votes[0].getText().replace(',',''))
+        'text': self.choices[0].getText(),
+        'percentage': self.percentages[0].getText(),
+        'votes': int(self.votes[0].getText().replace(',',''))
       },
       'choice_2': {
-        'text': choices[1].getText(),
-        'percentage': percentages[1].getText(),
-        'votes': int(votes[1].getText().replace(',',''))
+        'text': self.choices[1].getText(),
+        'percentage': self.percentages[1].getText(),
+        'votes': int(self.votes[1].getText().replace(',',''))
       }
     }
